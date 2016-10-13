81025a080d625648e6c6973d95379c407b5dafdd
AFaust <axel.faust@acosix.org>
2016-10-13 23:04:47 +0200

    Standardize Acosix open source license on Apache License 2.0

--- share/src/main/java/de/acosix/alfresco/utility/share/surf/Less4jCssThemeHandler.java
+++ share/src/main/java/de/acosix/alfresco/utility/share/surf/Less4jCssThemeHandler.java
@@ -1,15 +1,22 @@
 /*
  * Copyright 2016 Acosix GmbH
  *
- * Licensed under the Eclipse Public License (EPL), Version 1.0 (the "License"); you may not use
- * this file except in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * https://www.eclipse.org/legal/epl-v10.html
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions
- * and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * Linked to Alfresco
+ * Original file alfresco-repository/source/java/org/alfresco/repo/admin/Log4JHierarchyInit.java
+ * Copyright (C) 2005-2010 Alfresco Software Limited.
  */
 package de.acosix.alfresco.utility.share.surf;
 
