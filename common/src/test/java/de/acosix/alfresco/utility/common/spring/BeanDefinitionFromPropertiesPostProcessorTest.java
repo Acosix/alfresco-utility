1e88150b1ae98ae19a8d30e8d5764424852d3751
AFaust <axel.faust@acosix.org>
2016-11-16 12:39:49 +0100

    Expand tests for BeanDefinitionFromPropertiesPostProcessor

--- common/src/test/java/de/acosix/alfresco/utility/common/spring/BeanDefinitionFromPropertiesPostProcessorTest.java
+++ common/src/test/java/de/acosix/alfresco/utility/common/spring/BeanDefinitionFromPropertiesPostProcessorTest.java
@@ -15,9 +15,11 @@
  */
 package de.acosix.alfresco.utility.common.spring;
 
-import org.alfresco.util.exec.RuntimeExec;
+import java.util.Arrays;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.internal.ArrayComparisonFailure;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -30,19 +32,93 @@ public class BeanDefinitionFromPropertiesPostProcessorTest
 {
 
     @Test
-    public void simpleBean()
+    public void singleton()
     {
         try (final ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "classpath:BeanDefinitionFromPropertiesPostProcessorTest-simpleBean-context.xml"))
+                "classpath:BeanDefinitionFromPropertiesPostProcessorTest/singleton-context.xml"))
         {
-            final Object simpleBean = context.getBean("beanTypeX.simpleBean");
+            final TestDummyBean simpleBean = context.getBean("beanTypeX.simpleBean", TestDummyBean.class);
 
-            // TODO Switch with other simple bean class that provides a simple property that can be set AND get
-            Assert.assertTrue("beanTypeX.simpleBean is not of expected type", simpleBean instanceof RuntimeExec);
-            Assert.assertArrayEquals("command is not of expected array value", new String[] { "dummy", "cmd" },
-                    ((RuntimeExec) simpleBean).getCommand());
+            final TestDummyBean refBean1 = context.getBean("beanTypeX.refBean1", TestDummyBean.class);
+            final TestDummyBean refBean2 = context.getBean("beanTypeX.refBean2", TestDummyBean.class);
+            final TestDummyBean refBean3 = context.getBean("beanTypeX.refBean3", TestDummyBean.class);
+
+            this.verifyBean(simpleBean, refBean1, refBean2, refBean3);
         }
     }
 
-    // TODO Test constructs with all types of properties
+    @Test
+    public void prototype()
+    {
+        try (final ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "classpath:BeanDefinitionFromPropertiesPostProcessorTest/proto-context.xml"))
+        {
+            final TestDummyBean instance1 = context.getBean("beanTypeX.simpleBean.proto", TestDummyBean.class);
+            final TestDummyBean instance2 = context.getBean("beanTypeX.simpleBean.proto", TestDummyBean.class);
+
+            final TestDummyBean refBean1 = context.getBean("beanTypeX.refBean1", TestDummyBean.class);
+            final TestDummyBean refBean2 = context.getBean("beanTypeX.refBean2", TestDummyBean.class);
+            final TestDummyBean refBean3 = context.getBean("beanTypeX.refBean3", TestDummyBean.class);
+
+            Assert.assertFalse("instance1 is not a different object than instance 2", instance1 == instance2);
+
+            this.verifyBean(instance1, refBean1, refBean2, refBean3);
+            this.verifyBean(instance2, refBean1, refBean2, refBean3);
+        }
+    }
+
+    @Test
+    public void abstractParent()
+    {
+        try (final ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "classpath:BeanDefinitionFromPropertiesPostProcessorTest/abstractParent-context.xml"))
+        {
+            final TestDummyBean concreteInstance = context.getBean("beanTypeX.simpleBean.withParent", TestDummyBean.class);
+
+            final TestDummyBean refBean1 = context.getBean("beanTypeX.refBean1", TestDummyBean.class);
+            final TestDummyBean refBean2 = context.getBean("beanTypeX.refBean2", TestDummyBean.class);
+            final TestDummyBean refBean3 = context.getBean("beanTypeX.refBean3", TestDummyBean.class);
+
+            this.verifyBean(concreteInstance, refBean1, refBean2, refBean3);
+        }
+    }
+
+    // TODO Test constructs with override / removal of predefined beans
+
+    protected void verifyBean(final TestDummyBean beanToVerify, final TestDummyBean refBean1, final TestDummyBean refBean2,
+            final TestDummyBean refBean3) throws ArrayComparisonFailure
+    {
+        Assert.assertArrayEquals("stringList does not contain expected values", new String[] { "value 1", "value 2" },
+                beanToVerify.getStringList().toArray(new String[0]));
+        Assert.assertArrayEquals("numberList does not contain expected values", new Integer[] { Integer.valueOf(1), Integer.valueOf(2) },
+                beanToVerify.getIntegerList().toArray(new Integer[0]));
+        Assert.assertArrayEquals("booleanList does not contain expected values", new Boolean[] { Boolean.TRUE, Boolean.FALSE },
+                beanToVerify.getBooleanList().toArray(new Boolean[0]));
+        Assert.assertArrayEquals("beanList does not contain expected bean", new Object[] { refBean1, refBean2 },
+                beanToVerify.getBeanList().toArray(new Object[0]));
+
+        Assert.assertTrue("stringMap does not contain expected keys",
+                beanToVerify.getStringMap().keySet().containsAll(Arrays.asList("strkey1", "strkey2")));
+        Assert.assertEquals("stringMap[strkey1] does match expected value", "value 1", beanToVerify.getStringMap().get("strkey1"));
+        Assert.assertEquals("stringMap[strkey2] does match expected value", "value 2", beanToVerify.getStringMap().get("strkey2"));
+        Assert.assertTrue("integerMap does not contain expected keys",
+                beanToVerify.getIntegerMap().keySet().containsAll(Arrays.asList("intkey1", "intkey2")));
+        Assert.assertEquals("integerMap[intkey1] does match expected value", Integer.valueOf(1),
+                beanToVerify.getIntegerMap().get("intkey1"));
+        Assert.assertEquals("integerMap[intkey2] does match expected value", Integer.valueOf(2),
+                beanToVerify.getIntegerMap().get("intkey2"));
+        Assert.assertTrue("booleanMap does not contain expected keys",
+                beanToVerify.getBooleanMap().keySet().containsAll(Arrays.asList("boolkey1", "boolkey2")));
+        Assert.assertEquals("booleanMap[boolkey1] does match expected value", Boolean.TRUE, beanToVerify.getBooleanMap().get("boolkey1"));
+        Assert.assertEquals("booleanMap[boolkey2] does match expected value", Boolean.FALSE, beanToVerify.getBooleanMap().get("boolkey2"));
+        Assert.assertTrue("beanMap does not contain expected keys",
+                beanToVerify.getBeanMap().keySet().containsAll(Arrays.asList("beankey1", "beankey2")));
+        Assert.assertSame("beanMap[beankey1] does match expected bean", refBean1, beanToVerify.getBeanMap().get("beankey1"));
+        Assert.assertSame("beanMap[beankey2] does match expected bean", refBean2, beanToVerify.getBeanMap().get("beankey2"));
+
+        Assert.assertEquals("stringValue does not match expected value", "dummy", beanToVerify.getStringValue());
+        Assert.assertEquals("numberValue does not match expected value", Integer.valueOf(3), beanToVerify.getIntegerValue());
+        Assert.assertEquals("booleanValue does not match expected value", Boolean.TRUE, beanToVerify.getBooleanValue());
+        Assert.assertSame("beanReference does not match expected bean", refBean3, beanToVerify.getBeanReference());
+    }
 }
---
+++ common/src/test/java/de/acosix/alfresco/utility/common/spring/TestDummyBean.java
@@ -0,0 +1,257 @@
+/*
+ * Copyright 2016 Acosix GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.acosix.alfresco.utility.common.spring;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Instances of this class are used as dummy beans for unit tests.
+ *
+ * @author Axel Faust, <a href="http://acosix.de">Acosix GmbH</a>
+ */
+public class TestDummyBean
+{
+
+    private List<String> stringList;
+
+    private List<Integer> integerList;
+
+    private List<Boolean> booleanList;
+
+    private List<Object> beanList;
+
+    private Map<String, String> stringMap;
+
+    private Map<String, Integer> integerMap;
+
+    private Map<String, Boolean> booleanMap;
+
+    private Map<String, Object> beanMap;
+
+    private String stringValue;
+
+    private Integer integerValue;
+
+    private Boolean booleanValue;
+
+    private Object beanReference;
+
+    /**
+     * @return the stringList
+     */
+    public List<String> getStringList()
+    {
+        return this.stringList;
+    }
+
+    /**
+     * @param stringList
+     *            the stringList to set
+     */
+    public void setStringList(final List<String> stringList)
+    {
+        this.stringList = stringList;
+    }
+
+    /**
+     * @return the integerList
+     */
+    public List<Integer> getIntegerList()
+    {
+        return this.integerList;
+    }
+
+    /**
+     * @param integerList
+     *            the integerList to set
+     */
+    public void setIntegerList(final List<Integer> integerList)
+    {
+        this.integerList = integerList;
+    }
+
+    /**
+     * @return the booleanList
+     */
+    public List<Boolean> getBooleanList()
+    {
+        return this.booleanList;
+    }
+
+    /**
+     * @param booleanList
+     *            the booleanList to set
+     */
+    public void setBooleanList(final List<Boolean> booleanList)
+    {
+        this.booleanList = booleanList;
+    }
+
+    /**
+     * @return the beanList
+     */
+    public List<Object> getBeanList()
+    {
+        return this.beanList;
+    }
+
+    /**
+     * @param beanList
+     *            the beanList to set
+     */
+    public void setBeanList(final List<Object> beanList)
+    {
+        this.beanList = beanList;
+    }
+
+    /**
+     * @return the stringMap
+     */
+    public Map<String, String> getStringMap()
+    {
+        return this.stringMap;
+    }
+
+    /**
+     * @param stringMap
+     *            the stringMap to set
+     */
+    public void setStringMap(final Map<String, String> stringMap)
+    {
+        this.stringMap = stringMap;
+    }
+
+    /**
+     * @return the integerMap
+     */
+    public Map<String, Integer> getIntegerMap()
+    {
+        return this.integerMap;
+    }
+
+    /**
+     * @param integerMap
+     *            the integerMap to set
+     */
+    public void setIntegerMap(final Map<String, Integer> integerMap)
+    {
+        this.integerMap = integerMap;
+    }
+
+    /**
+     * @return the booleanMap
+     */
+    public Map<String, Boolean> getBooleanMap()
+    {
+        return this.booleanMap;
+    }
+
+    /**
+     * @param booleanMap
+     *            the booleanMap to set
+     */
+    public void setBooleanMap(final Map<String, Boolean> booleanMap)
+    {
+        this.booleanMap = booleanMap;
+    }
+
+    /**
+     * @return the beanMap
+     */
+    public Map<String, Object> getBeanMap()
+    {
+        return this.beanMap;
+    }
+
+    /**
+     * @param beanMap
+     *            the beanMap to set
+     */
+    public void setBeanMap(final Map<String, Object> beanMap)
+    {
+        this.beanMap = beanMap;
+    }
+
+    /**
+     * @return the stringValue
+     */
+    public String getStringValue()
+    {
+        return this.stringValue;
+    }
+
+    /**
+     * @param stringValue
+     *            the stringValue to set
+     */
+    public void setStringValue(final String stringValue)
+    {
+        this.stringValue = stringValue;
+    }
+
+    /**
+     * @return the integerValue
+     */
+    public Integer getIntegerValue()
+    {
+        return this.integerValue;
+    }
+
+    /**
+     * @param integerValue
+     *            the integerValue to set
+     */
+    public void setIntegerValue(final Integer integerValue)
+    {
+        this.integerValue = integerValue;
+    }
+
+    /**
+     * @return the booleanValue
+     */
+    public Boolean getBooleanValue()
+    {
+        return this.booleanValue;
+    }
+
+    /**
+     * @param booleanValue
+     *            the booleanValue to set
+     */
+    public void setBooleanValue(final Boolean booleanValue)
+    {
+        this.booleanValue = booleanValue;
+    }
+
+    /**
+     * @return the beanReference
+     */
+    public Object getBeanReference()
+    {
+        return this.beanReference;
+    }
+
+    /**
+     * @param beanReference
+     *            the beanReference to set
+     */
+    public void setBeanReference(final Object beanReference)
+    {
+        this.beanReference = beanReference;
+    }
+
+}
