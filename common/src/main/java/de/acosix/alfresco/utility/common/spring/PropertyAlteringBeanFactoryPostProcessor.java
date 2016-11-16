fd5e96b58a70af30fdc48c21afcd7ad7d788b7a8
AFaust <axel.faust@acosix.org>
2016-11-16 14:10:23 +0100

    Merge map handling into existing property alteration post processor

--- common/src/main/java/de/acosix/alfresco/utility/common/spring/PropertyAlteringBeanFactoryPostProcessor.java
+++ common/src/main/java/de/acosix/alfresco/utility/common/spring/PropertyAlteringBeanFactoryPostProcessor.java
@@ -16,7 +16,10 @@
 package de.acosix.alfresco.utility.common.spring;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.Function;
 
 import org.slf4j.Logger;
@@ -30,6 +33,7 @@ import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.support.ManagedList;
+import org.springframework.beans.factory.support.ManagedMap;
 
 /**
  * {@link BeanFactoryPostProcessor Bean factory post processor} to alter a property of a bean definition with adapted configuration before
@@ -50,15 +54,17 @@ public class PropertyAlteringBeanFactoryPostProcessor implements BeanFactoryPost
 
     protected String propertyName;
 
+    protected Object value;
+
     protected String beanReferenceName;
+
+    protected List<Object> values;
 
     protected List<String> beanReferenceNames;
 
-    // can only handle simple values, no maps (except via bean references)
+    protected Map<Object, Object> mappedValues;
 
-    protected Object value;
-
-    protected List<Object> values;
+    protected Map<Object, String> mappedBeanReferenceNames;
 
     protected boolean addAsFirst = false;
 
@@ -105,12 +111,30 @@ public class PropertyAlteringBeanFactoryPostProcessor implements BeanFactoryPost
     }
 
     /**
+     * @param value
+     *            the value to set
+     */
+    public void setValue(final Object value)
+    {
+        this.value = value;
+    }
+
+    /**
      * @param beanReferenceName
      *            the beanReferenceName to set
      */
     public void setBeanReferenceName(final String beanReferenceName)
     {
         this.beanReferenceName = beanReferenceName;
+    }
+
+    /**
+     * @param values
+     *            the values to set
+     */
+    public void setValues(final List<Object> values)
+    {
+        this.values = values;
     }
 
     /**
@@ -123,21 +147,21 @@ public class PropertyAlteringBeanFactoryPostProcessor implements BeanFactoryPost
     }
 
     /**
-     * @param value
-     *            the value to set
+     * @param mappedValues
+     *            the mappedValues to set
      */
-    public void setValue(final Object value)
+    public void setMappedValues(final Map<Object, Object> mappedValues)
     {
-        this.value = value;
+        this.mappedValues = mappedValues;
     }
 
     /**
-     * @param values
-     *            the values to set
+     * @param mappedBeanReferenceNames
+     *            the mappedBeanReferenceNames to set
      */
-    public void setValues(final List<Object> values)
+    public void setMappedBeanReferenceNames(final Map<Object, String> mappedBeanReferenceNames)
     {
-        this.values = values;
+        this.mappedBeanReferenceNames = mappedBeanReferenceNames;
     }
 
     /**
@@ -212,70 +236,11 @@ public class PropertyAlteringBeanFactoryPostProcessor implements BeanFactoryPost
 
             if (this.values != null || this.beanReferenceNames != null)
             {
-                LOGGER.debug("[{}] List of values / bean reference names has been configured - treating property {} of {} as <list>",
-                        this.beanName, this.propertyName, this.targetBeanName);
-
-                final ManagedList<Object> list = new ManagedList<>();
-
-                if (this.merge && configuredValue != null)
-                {
-                    final Object configuredValueDefinition = configuredValue.getValue();
-                    if (configuredValueDefinition instanceof ManagedList<?>)
-                    {
-                        final ManagedList<?> oldList = (ManagedList<?>) configuredValueDefinition;
-                        list.setElementTypeName(oldList.getElementTypeName());
-                        list.setMergeEnabled(oldList.isMergeEnabled());
-                        list.setSource(oldList.getSource());
-
-                        list.addAll(oldList);
-
-                        LOGGER.debug("[{}] Merged existing value list values: {}", this.beanName, oldList);
-                    }
-                }
-
-                List<Object> valuesToAdd;
-                if (this.values != null)
-                {
-                    LOGGER.debug("[{}] List of configured values for {} of {}: ", this.beanName, this.propertyName, this.targetBeanName,
-                            this.values);
-                    valuesToAdd = this.values;
-                }
-                else
-                {
-                    LOGGER.debug("[{}] List of configured bean reference names for {} of {}: ", this.beanName, this.propertyName,
-                            this.targetBeanName, this.values);
-                    valuesToAdd = new ArrayList<>();
-                    for (final String beanReferenceName : this.beanReferenceNames)
-                    {
-                        valuesToAdd.add(new RuntimeBeanReference(beanReferenceName));
-                    }
-                }
-
-                if (this.addAsFirst)
-                {
-                    LOGGER.debug("[{}] Adding new entries at start of list for {} of {}", this.beanName, this.propertyName,
-                            this.targetBeanName);
-                    list.addAll(0, valuesToAdd);
-                }
-                else if (this.addAtIndex >= 0 && this.addAtIndex < list.size())
-                {
-                    LOGGER.debug("[{}] Adding new entries at position {} of list for {} of {}", this.beanName,
-                            String.valueOf(this.addAtIndex), this.propertyName, this.targetBeanName);
-                    list.addAll(this.addAtIndex, valuesToAdd);
-                }
-                else
-                {
-                    LOGGER.debug("[{}] Adding new entries at end of list for {} of {}", this.beanName, this.propertyName,
-                            this.targetBeanName);
-                    list.addAll(valuesToAdd);
-                }
-
-                if (!list.isMergeEnabled() && this.mergeParent)
-                {
-                    LOGGER.debug("[{}] Enabling \"merge\" for <list> on {} of {}", this.beanName, this.propertyName, this.targetBeanName);
-                }
-                list.setMergeEnabled(list.isMergeEnabled() || this.mergeParent);
-                value = list;
+                value = this.handleListValues(configuredValue);
+            }
+            else if (this.mappedValues != null || this.mappedBeanReferenceNames != null)
+            {
+                value = this.handleMapValues(configuredValue);
             }
             else if (this.value != null)
             {
@@ -308,5 +273,129 @@ public class PropertyAlteringBeanFactoryPostProcessor implements BeanFactoryPost
         {
             LOGGER.info("[{}] patch cannot be applied - no bean with name {} has been defined", this.beanName, this.targetBeanName);
         }
+    }
+
+    protected Object handleListValues(final PropertyValue configuredValue)
+    {
+        final Object value;
+        LOGGER.debug("[{}] List of values / bean reference names has been configured - treating property {} of {} as <list>", this.beanName,
+                this.propertyName, this.targetBeanName);
+
+        final ManagedList<Object> list = new ManagedList<>();
+
+        if (this.merge && configuredValue != null)
+        {
+            final Object configuredValueDefinition = configuredValue.getValue();
+            if (configuredValueDefinition instanceof ManagedList<?>)
+            {
+                final ManagedList<?> oldList = (ManagedList<?>) configuredValueDefinition;
+                list.setElementTypeName(oldList.getElementTypeName());
+                list.setMergeEnabled(oldList.isMergeEnabled());
+                list.setSource(oldList.getSource());
+
+                list.addAll(oldList);
+
+                LOGGER.debug("[{}] Merged existing value list values: {}", this.beanName, oldList);
+            }
+        }
+
+        List<Object> valuesToAdd;
+        if (this.values != null)
+        {
+            LOGGER.debug("[{}] List of configured values for {} of {}: ", this.beanName, this.propertyName, this.targetBeanName,
+                    this.values);
+            valuesToAdd = this.values;
+        }
+        else
+        {
+            LOGGER.debug("[{}] List of configured bean reference names for {} of {}: ", this.beanName, this.propertyName,
+                    this.targetBeanName, this.beanReferenceNames);
+            valuesToAdd = new ArrayList<>();
+            for (final String beanReferenceName : this.beanReferenceNames)
+            {
+                valuesToAdd.add(new RuntimeBeanReference(beanReferenceName));
+            }
+        }
+
+        if (this.addAsFirst)
+        {
+            LOGGER.debug("[{}] Adding new entries at start of list for {} of {}", this.beanName, this.propertyName, this.targetBeanName);
+            list.addAll(0, valuesToAdd);
+        }
+        else if (this.addAtIndex >= 0 && this.addAtIndex < list.size())
+        {
+            LOGGER.debug("[{}] Adding new entries at position {} of list for {} of {}", this.beanName, String.valueOf(this.addAtIndex),
+                    this.propertyName, this.targetBeanName);
+            list.addAll(this.addAtIndex, valuesToAdd);
+        }
+        else
+        {
+            LOGGER.debug("[{}] Adding new entries at end of list for {} of {}", this.beanName, this.propertyName, this.targetBeanName);
+            list.addAll(valuesToAdd);
+        }
+
+        if (!list.isMergeEnabled() && this.mergeParent)
+        {
+            LOGGER.debug("[{}] Enabling \"merge\" for <list> on {} of {}", this.beanName, this.propertyName, this.targetBeanName);
+        }
+        list.setMergeEnabled(list.isMergeEnabled() || this.mergeParent);
+        value = list;
+        return value;
+    }
+
+    protected Object handleMapValues(final PropertyValue configuredValue)
+    {
+        final Object value;
+        LOGGER.debug("[{}] Map of values / bean reference names has been configured - treating property {} of {} as <map>", this.beanName,
+                this.propertyName, this.targetBeanName);
+
+        final ManagedMap<Object, Object> map = new ManagedMap<>();
+
+        if (this.merge && configuredValue != null)
+        {
+            final Object configuredValueDefinition = configuredValue.getValue();
+            if (configuredValueDefinition instanceof ManagedMap<?, ?>)
+            {
+                final ManagedMap<?, ?> oldMap = (ManagedMap<?, ?>) configuredValueDefinition;
+
+                map.setKeyTypeName(oldMap.getKeyTypeName());
+                map.setValueTypeName(oldMap.getValueTypeName());
+                map.setMergeEnabled(oldMap.isMergeEnabled());
+                map.setSource(oldMap.getSource());
+
+                map.putAll(oldMap);
+
+                LOGGER.debug("[{}] Merged existing map values: {}", this.beanName, oldMap);
+            }
+        }
+
+        Map<Object, Object> valuesToMap;
+        if (this.mappedValues != null)
+        {
+            LOGGER.debug("[{}] Map of configured values for {} of {}: ", this.beanName, this.propertyName, this.targetBeanName,
+                    this.mappedValues);
+            valuesToMap = this.mappedValues;
+        }
+        else
+        {
+            LOGGER.debug("[{}] Map of configured bean reference names for {} of {}: ", this.beanName, this.propertyName,
+                    this.targetBeanName, this.mappedBeanReferenceNames);
+            valuesToMap = new HashMap<>();
+            for (final Entry<Object, String> beanReferenceEntry : this.mappedBeanReferenceNames.entrySet())
+            {
+                valuesToMap.put(beanReferenceEntry.getKey(), new RuntimeBeanReference(beanReferenceEntry.getValue()));
+            }
+        }
+
+        LOGGER.debug("[{}] Putting new entries into map list for {} of {}", this.beanName, this.propertyName, this.targetBeanName);
+        map.putAll(valuesToMap);
+
+        if (!map.isMergeEnabled() && this.mergeParent)
+        {
+            LOGGER.debug("[{}] Enabling \"merge\" for <map> on {} of {}", this.beanName, this.propertyName, this.targetBeanName);
+        }
+        map.setMergeEnabled(map.isMergeEnabled() || this.mergeParent);
+        value = map;
+        return value;
     }
 }
