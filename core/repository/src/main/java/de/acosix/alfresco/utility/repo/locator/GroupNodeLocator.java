75415d86f22e01c1910db967d6c58ab1a7713b4a
AFaust <axel.faust@acosix.org>
2021-09-04 19:01:26 +0200

    Authority picker with enhanced search
    - search in configurable group (via startLocation group resolver and startLocationParams)
    - search in zone (via generic zone URL query argument)

---
+++ core/repository/src/main/java/de/acosix/alfresco/utility/repo/locator/GroupNodeLocator.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2016 - 2021 Acosix GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.acosix.alfresco.utility.repo.locator;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.alfresco.error.AlfrescoRuntimeException;
+import org.alfresco.repo.action.ParameterDefinitionImpl;
+import org.alfresco.repo.nodelocator.AbstractNodeLocator;
+import org.alfresco.repo.security.authority.AuthorityDAO;
+import org.alfresco.service.cmr.action.ParameterDefinition;
+import org.alfresco.service.cmr.dictionary.DataTypeDefinition;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.datatype.DefaultTypeConverter;
+import org.alfresco.service.cmr.security.AuthorityType;
+import org.alfresco.util.ParameterCheck;
+import org.alfresco.util.PropertyCheck;
+import org.springframework.beans.factory.InitializingBean;
+
+public class GroupNodeLocator extends AbstractNodeLocator implements InitializingBean
+{
+
+    public static final String NAME = "group";
+
+    public static final String PARAM_NAME = "name";
+
+    protected AuthorityDAO authorityDAO;
+
+    /**
+     *
+     * {@inheritDoc}
+     */
+    @Override
+    public void afterPropertiesSet()
+    {
+        PropertyCheck.mandatory(this, "authorityDAO", this.authorityDAO);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeRef getNode(final NodeRef source, final Map<String, Serializable> params)
+    {
+        // source NodeRef is ignored by this locator - resolution occurs exclusively based on parameters
+        String name = DefaultTypeConverter.INSTANCE.convert(String.class, params.get(PARAM_NAME));
+        ParameterCheck.mandatoryString("name", name);
+
+        final AuthorityType authorityType = AuthorityType.getAuthorityType(name);
+        // users are the only type without a prefix
+        if (authorityType == AuthorityType.USER)
+        {
+            name = AuthorityType.GROUP.getPrefixString() + name;
+        }
+        else if (authorityType != AuthorityType.GROUP)
+        {
+            throw new AlfrescoRuntimeException("Name " + name + " does not denote a group");
+        }
+
+        return this.authorityDAO.getAuthorityNodeRefOrNull(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<ParameterDefinition> getParameterDefinitions()
+    {
+        final List<ParameterDefinition> paramDefs = new ArrayList<>(2);
+        paramDefs.add(new ParameterDefinitionImpl(PARAM_NAME, DataTypeDefinition.TEXT, true, "Group Name"));
+        return paramDefs;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getName()
+    {
+        return NAME;
+    }
+
+    /**
+     * @param authorityDAO
+     *     the authorityDAO to set
+     */
+    public void setAuthorityDAO(final AuthorityDAO authorityDAO)
+    {
+        this.authorityDAO = authorityDAO;
+    }
+
+}
--- core/repository/src/main/webscripts/de/acosix/acosix-utility/forms/pickerChildren.get.js
+++ core/repository/src/main/webscripts/de/acosix/acosix-utility/forms/pickerChildren.get.js
@@ -10,12 +10,288 @@ function resolveNode(reference)
     return utils.resolveNodeReference(reference);
 }
 
+function findCategories(results, nodeRef, searchTerm, selectableType, maxResults)
+{
+    var aspect, categories, parent, rootCategories, rootNode, idx;
+
+    aspect = String(args.aspect || 'cm:generalclassifiable');
+    categories = [];
+
+    if (aspect === 'cm:taggable' || nodeRef === 'workspace://SpacesStore/tag:tag-root')
+    {
+        parent = resolveNode(nodeRef || 'workspace://SpacesStore/tag:tag-root');
+        categories = classification.getRootCategories('cm:taggable', searchTerm, maxResults, 0);
+    }
+    else
+    {
+        rootCategories = classification.getRootCategories(aspect, null, 1, 0);
+        if (rootCategories)
+        {
+            if (args.rootNode)
+            {
+                rootNode = resolveNode(args.rootNode) || rootCategories[0].parent;
+            }
+            else
+            {
+                rootNode = rootCategories[0].parent;
+            }
+
+            if (nodeRef === null || nodeRef === 'alfresco://category/root')
+            {
+                parent = rootNode;
+                if (searchTerm)
+                {
+                    categories = search.query({
+                        language: 'fts-alfresco',
+                        query: 'ANCESTOR:"' + parent.nodeRef + '" AND TYPE:"' + (selectableType || 'cm:category') + '" AND cm:name:"*' + searchTerm + '*"',
+                        page: {
+                            maxItems: maxResults,
+                            skipCount: 0
+                        },
+                        sort: [{
+                            column: '@cm:name',
+                            ascending: true
+                        }]
+                    });
+                }
+                else
+                {
+                    categories = classification.getRootCategories(aspect, null, maxResults, 0);
+                }
+            }
+            else
+            {
+                parent = resolveNode(nodeRef);
+                if (searchTerm)
+                {
+                    categories = search.query({
+                        language: 'fts-alfresco',
+                        query: 'ANCESTOR:"' + parent.nodeRef + '" AND TYPE:"' + (selectableType || 'cm:category') + '" AND cm:name:"*' + searchTerm + '*"',
+                        page: {
+                            maxItems: maxResults,
+                            skipCount: 0
+                        },
+                        sort: [{
+                            column: '@cm:name',
+                            ascending: true
+                        }]
+                    });
+                }
+                else
+                {
+                    categories = search.query({
+                        language: 'fts-alfresco',
+                        query: 'PARENT:"' + parent.nodeRef + '" AND TYPE:"' + (selectableType || 'cm:category') + '"',
+                        page: {
+                            maxItems: maxResults,
+                            skipCount: 0
+                        },
+                        sort: [{
+                            column: '@cm:name',
+                            ascending: true
+                        }]
+                    });
+                }
+            }
+        }
+    }
+
+    for (idx = 0; idx < categories.length; idx++)
+    {
+        results.push({
+            item: categories[idx],
+            selectable: categories[idx].isSubType(selectableType || 'cm:category')
+        });
+    }
+
+    model.parent = parent;
+    model.rootNode = rootNode;
+}
+
+function findUsers(results, nodeRef, searchTerm, maxResults)
+{
+    var ctxt, authorityService, zone, ancestorAuthority, node, personNodes, userNameSet, collator, nameCache, ensureCached;
+
+    ctxt = Packages.org.springframework.web.context.ContextLoader.getCurrentWebApplicationContext();
+    authorityService = ctxt.getBean('AuthorityService', Packages.org.alfresco.service.cmr.security.AuthorityService);
+
+    zone = args.zone ? String(args.zone) : null;
+    ancestorAuthority = null;
+    if (nodeRef)
+    {
+        node = search.findNode(nodeRef);
+        if (node.isSubType('cm:authorityContainer'))
+        {
+            ancestorAuthority = String(node.properties.authorityName);
+        }
+    }
+
+    // retrieves only refs, not actual nodes
+    personNodes = people.getPeople(searchTerm, zone === null && ancestorAuthority === null ? maxResults : -1, 'lastName', true);
+    personNodes = personNodes.map(function (ref)
+    {
+        return search.findNode(ref);
+    });
+
+    // zone is a cheaper filter than ancestorAuthority, so apply first to reduce cost for second
+    if (zone !== null)
+    {
+        personNodes = personNodes.filter(function (p)
+        {
+            p.parentAssocs.inZone.some(function (z)
+            {
+                return String(z.name) === zone;
+            });
+        });
+    }
+
+    if (ancestorAuthority !== null)
+    {
+        userNameSet = authorityService.findAuthorities(Packages.org.alfresco.service.cmr.security.AuthorityType.USER, ancestorAuthority, false, null, zone);
+        personNodes = personNodes.filter(function (p)
+        {
+            return userNameSet.contains(p.properties.userName);
+        });
+    }
+
+    collator = Packages.java.text.Collator.getInstance(Packages.org.springframework.extensions.surf.util.I18NUtil.getLocale());
+    nameCache = {};
+    ensureCached = function (p)
+    {
+        if (!nameCache.hasOwnProperty(p.properties.userName))
+        {
+            nameCache[p.properties.userName] = String(p.properties.lastName || p.properties.userName);
+            if (p.properties.lastName && p.properties.firstName)
+            {
+                nameCache[p.properties.userName] += ' ' + p.properties.firstName;
+            }
+            nameCache[p.properties.userName] = nameCache[p.properties.userName].toLowerCase();
+        }
+    };
+    personNodes.sort(function (a, b)
+    {
+        ensureCached(a);
+        ensureCached(b);
+        return collator.compare(nameCache[a.properties.userName], nameCache[b.properties.userName]);
+    });
+
+    if (personNodes.length > maxResults) 
+    {
+        personNodes.splice(maxResults, personNodes.length - maxResults);
+    }
+
+    personNodes.forEach(function (p)
+    {
+        results.push({
+            item: {
+                typeShort : p.typeShort,
+                isContainer: false,
+                properties: {
+                    userName: p.properties.userName,
+                    name: (p.properties.firstName ? (p.properties.firstName + ' ') : '') + (p.properties.lastName ? (p.properties.lastName + ' ') : '') + '(' + p.properties.userName + ')',
+                    jobtitle: p.properties.jobtitle || ''
+                },
+                displayPath: p.displayPath,
+                nodeRef: '' + p.nodeRef
+            },
+            selectable: people.isAccountEnabled(p.properties.userName)
+        });
+    });
+}
+
+function findGroups(results, nodeRef, searchTerm, maxResults)
+{
+    var ctxt, authorityService, zone, node, ancestorAuthority, authorityNameSet, authorityNameIter, authorityName, resultGroups, collator, nameCache, ensureCached;
+
+    ctxt = Packages.org.springframework.web.context.ContextLoader.getCurrentWebApplicationContext();
+    authorityService = ctxt.getBean('AuthorityService', Packages.org.alfresco.service.cmr.security.AuthorityService);
+
+    zone = String(args.zone || 'APP.DEFAULT');
+    ancestorAuthority = null;
+    if (nodeRef)
+    {
+        node = search.findNode(nodeRef);
+        if (node.isSubType('cm:authorityContainer'))
+        {
+            ancestorAuthority = String(node.properties.authorityName);
+        }
+    }
+
+    if (searchTerm !== null && (searchTerm.length === 0 || searchTerm === '*'))
+    {
+        searchTerm = null;
+    }
+
+    resultGroups = [];
+    authorityNameSet = authorityService.findAuthorities(Packages.org.alfresco.service.cmr.security.AuthorityType.GROUP, ancestorAuthority, false, searchTerm, zone);
+    authorityNameIter = authorityNameSet.iterator();
+    while (authorityNameIter.hasNext())
+    {
+        authorityName = authorityNameIter.next();
+        resultGroups.push(groups.getGroup(authorityName));
+    }
+
+    collator = Packages.java.text.Collator.getInstance(Packages.org.springframework.extensions.surf.util.I18NUtil.getLocale());
+    nameCache = {};
+    ensureCached = function (g)
+    {
+        if (!nameCache.hasOwnProperty(g.fullName))
+        {
+            nameCache[g.fullName] = String(g.displayName || g.shortName).toLowerCase();
+        }
+    };
+    resultGroups.sort(function (a, b)
+    {
+        ensureCached(a);
+        ensureCached(b);
+        return collator.compare(nameCache[a.fullName], nameCache[b.fullName]);
+    });
+
+    if (resultGroups.length > maxResults - results.length)
+    {
+        resultGroups.splice(maxResults - results.length, resultGroups.length - (maxResults - results.length));
+    }
+
+    resultGroups.forEach(function (group)
+    {
+        results.push({
+            item: {
+                typeShort : group.groupNode.typeShort,
+                isContainer: false,
+                properties: {
+                    name: group.displayName || group.shortName
+                },
+                displayPath: group.groupNode.displayPath,
+                nodeRef: '' + group.groupNode.nodeRef
+            },
+            selectable: true
+        });
+    });
+}
+
+function findAuthorities(results, nodeRef, searchTerm, selectableType, maxResults)
+{
+    if (selectableType === 'cm:person')
+    {
+        findUsers(results, nodeRef, searchTerm, maxResults);
+    }
+    else if (selectableType === 'cm:authorityContainer')
+    {
+        findGroups(results, nodeRef, searchTerm, maxResults);
+    }
+    else
+    {
+        findGroups(results, nodeRef, searchTerm, maxResults);
+        findUsers(results, nodeRef, searchTerm, maxResults);
+    }
+}
+
 function main()
 {
-    var itemType, parent, rootNode, results, nodeRef, xPathNodes, maxResults, searchTerm, selectableType, aspect, categories, rootCategories, idx;
+    var itemType, results, nodeRef, xPathNodes, maxResults, searchTerm, selectableType;
 
-    parent = null;
-    rootNode = companyhome;
+    model.parent = null;
+    model.rootNode = companyhome;
     results = [];
     try
     {
@@ -43,95 +319,11 @@ function main()
 
         if (itemType === 'category')
         {
-            aspect = String(args.aspect || 'cm:generalclassifiable');
-            categories = [];
-        
-            if (aspect === 'cm:taggable' || nodeRef === 'workspace://SpacesStore/tag:tag-root')
-            {
-                parent = resolveNode(nodeRef || 'workspace://SpacesStore/tag:tag-root');
-                categories = classification.getRootCategories('cm:taggable', searchTerm, maxResults, 0);
-            }
-            else
-            {
-                rootCategories = classification.getRootCategories(aspect, null, 1, 0);
-                if (rootCategories)
-                {
-                    if (args.rootNode)
-                    {
-                        rootNode = resolveNode(args.rootNode) || rootCategories[0].parent;
-                    }
-                    else
-                    {
-                        rootNode = rootCategories[0].parent;
-                    }
-
-                    if (nodeRef === null || nodeRef === 'alfresco://category/root')
-                    {
-                        parent = rootNode;
-                        if (searchTerm)
-                        {
-                            categories = search.query({
-                                language: 'fts-alfresco',
-                                query: 'ANCESTOR:"' + parent.nodeRef + '" AND TYPE:"' + (selectableType || 'cm:category') + '" AND cm:name:"*' + searchTerm + '*"',
-                                page: {
-                                    maxItems: maxResults,
-                                    skipCount: 0
-                                },
-                                sort: [{
-                                    column: '@cm:name',
-                                    ascending: true
-                                }]
-                            });
-                        }
-                        else
-                        {
-                            categories = classification.getRootCategories(aspect, null, maxResults, 0);
-                        }
-                    }
-                    else
-                    {
-                        parent = resolveNode(nodeRef);
-                        if (searchTerm)
-                        {
-                            categories = search.query({
-                                language: 'fts-alfresco',
-                                query: 'ANCESTOR:"' + parent.nodeRef + '" AND TYPE:"' + (selectableType || 'cm:category') + '" AND cm:name:"*' + searchTerm + '*"',
-                                page: {
-                                    maxItems: maxResults,
-                                    skipCount: 0
-                                },
-                                sort: [{
-                                    column: '@cm:name',
-                                    ascending: true
-                                }]
-                            });
-                        }
-                        else
-                        {
-                            categories = search.query({
-                                language: 'fts-alfresco',
-                                query: 'PARENT:"' + parent.nodeRef + '" AND TYPE:"' + (selectableType || 'cm:category') + '"',
-                                page: {
-                                    maxItems: maxResults,
-                                    skipCount: 0
-                                },
-                                sort: [{
-                                    column: '@cm:name',
-                                    ascending: true
-                                }]
-                            });
-                        }
-                    }
-                }
-            }
-
-            for (idx = 0; idx < categories.length; idx++)
-            {
-                results.push({
-                    item: categories[idx],
-                    selectable: categories[idx].isSubType(selectableType || 'cm:category')
-                });
-            }
+            findCategories(results, nodeRef, searchTerm, selectableType, maxResults);
+        }
+        else if (itemType === 'authority')
+        {
+            findAuthorities(results, nodeRef, searchTerm, selectableType, maxResults);
         }
         else
         {
@@ -143,8 +335,7 @@ function main()
         status.setCode(500, e.message);
     }
 
-    model.parent = parent;
-    model.rootNode = rootNode;
+    
     model.results = results;
 }
 
