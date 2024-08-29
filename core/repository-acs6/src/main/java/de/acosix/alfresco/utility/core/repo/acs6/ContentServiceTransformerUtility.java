e6852407b1a3e7848a69c3a69c4cddc4bcc441db
AFaust <axel.faust@acosix.org>
2024-08-29 13:08:31 +0200

    #14: more specific catch block to tame FindBugs

--- core/repository-acs6/src/main/java/de/acosix/alfresco/utility/core/repo/acs6/ContentServiceTransformerUtility.java
+++ core/repository-acs6/src/main/java/de/acosix/alfresco/utility/core/repo/acs6/ContentServiceTransformerUtility.java
@@ -51,7 +51,7 @@ public class ContentServiceTransformerUtility
                 }
             }
         }
-        catch (final Exception ignore)
+        catch (final ClassNotFoundException | SecurityException ignore)
         {
             // ignored
         }
